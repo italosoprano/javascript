deef36a3bdbc9167e2eccec6a8b13ace4c541199
Ítalo Soprano <110266373+italosoprano@users.noreply.github.com>
2022-09-19 23:41:17 -0400

    Primeira aula de if e else

---
+++ Exercicios/ex005/ex008.js
@@ -0,0 +1,6 @@
+var vel = 50
+console.log(`A velocidade do seu carro é igual a ${vel} km/h!!!`)
+if(vel > 60) { //Condição simples.
+    console.log('você ultrapassou a velocidade permitida, MULTADO!!!')
+}
+console.log('Usa o cinto seu arrombado!!!')
---
+++ Exercicios/ex005/ex009.js
@@ -0,0 +1,7 @@
+var país = 'Brasil'
+
+if(país == 'Brasil'){
+    console.log('Eaae seu fudido!!!')
+} else {
+    console.log('Calate!!! Seu gringuito de mierda!!!')
+} 
