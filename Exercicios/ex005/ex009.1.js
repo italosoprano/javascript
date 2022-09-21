8cc95c57dc7c0f751f0d69b8bd5db6b26523fd68
Ítalo Soprano <110266373+italosoprano@users.noreply.github.com>
2022-09-20 22:17:54 -0400

    atualização

---
+++ Exercicios/ex005/ex009.1.js
@@ -0,0 +1,11 @@
+var idade = 16
+
+if (idade > 18) {
+    console.log('Maior de idade votante!!')
+} else {
+    if (idade >= 16) {
+        console.log('Menor de Idade votante!!')
+    } else {
+        console.log('Menor de idade que não vota!!')
+    }
+}
