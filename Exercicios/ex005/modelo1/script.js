311357036abadc4ee9f55b7e146eb87e891b7bb0
Ítalo Soprano <110266373+italosoprano@users.noreply.github.com>
2022-09-23 00:12:14 -0400

    atualização

--- Exercicios/ex005/modelo1/script.js
+++ Exercicios/ex005/modelo1/script.js
@@ -1,7 +1,7 @@
 function carregar() {
-    var msg = window.document.getElementById('msg')
-    var foto = windo.document.getElementById('foto')
     var data = new Date()
     var hora = data.getHours()
+    var msg = window.document.getElementById("msg")
+    var img = window.document.getElementById("imagem")
     msg.innerHTML = `Agora são ${hora} horas.`
 }
