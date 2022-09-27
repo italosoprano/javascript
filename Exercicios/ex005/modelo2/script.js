a1c10c1e4ce25b5d7addda2b8a97d017ead4a63c
Ítalo Soprano <110266373+italosoprano@users.noreply.github.com>
2022-09-26 22:45:37 -0400

    console.log('atualização')

--- Exercicios/ex005/modelo2/script.js
+++ Exercicios/ex005/modelo2/script.js
@@ -1,1 +1,19 @@
-alert('eaae')
+function checar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+    if(fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO!!!]')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        if (fsex[0].checked) {
+            genero = 'masculino'
+        } else if (fsex[1].checked) {
+            genero = 'feminino'
+        }
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+    }
+} 
