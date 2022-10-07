0bc0e36b7e9eca57d46269b0b231b18367e13ad9
Ítalo Soprano <110266373+italosoprano@users.noreply.github.com>
2022-10-06 22:01:56 -0400

    console.log('atualizar')

--- Exercicios/ex006/modelo4/script.js
+++ Exercicios/ex006/modelo4/script.js
@@ -1,20 +1,18 @@
 function tabuada() {
-    var num = document.getElementById('txtn')
-    var tabuada = document.getElementById('seltab')
-    
-    if(num.value.length == 0) {
-        window.alert('Por favor, digite um numero!!!')
+    var numero = document.getElementById('txtn')
+    var tab = document.getElementById('seltab')
+
+    if(numero.value.length == 0) {
+        window.alert('Por favor, escolha um numero.')
     } else {
-        let n = Number(num.value)
+        let n = Number(numero.value)
         let c = 1
-        tabuada.innerHTML = ' '
-        while(c <= 10) {
+        tab.innerHTML = ' '
+
+        for (c = 1; c <= 10 ; c++) {
             let item = document.createElement('option')
             item.text = `${n} x ${c} = ${n*c}`
-            item.value = `tab${c}`
-            tabuada.appendChild(item)
-            c++
+            tab.appendChild(item)
         }
     }
-    
 }
